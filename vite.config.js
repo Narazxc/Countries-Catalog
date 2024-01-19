f7636c0af6f67939ce7cb2084888d4c4bf084c0e
Sitha Sovannara <sithasovannara51@gmail.com>
2024-01-20 01:44:45 +0700

    Deploy attempt #3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/Countries-Catalog/",
+  plugins: [react()],
 });
